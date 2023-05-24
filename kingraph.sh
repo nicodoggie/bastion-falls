a257f8b9e5857492a62622a597a74c544ba71285
ensu <enrico.suarez@xsplit.com>
2023-05-24 17:00:03 +0800

    feat: added family tree graphing tool kingraph

--- kingraph.sh
+++ kingraph.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 yarn kingraph content/families/savoy/family.yaml -F svg
+yarn kingraph content/families/mcfarland/family.yaml -F svg
+yarn kingraph content/families/durant/family.yaml -F svg
