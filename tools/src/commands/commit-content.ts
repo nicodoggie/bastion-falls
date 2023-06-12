e5d12f52863b55e70043a9f6feac4b32798b8fc1
ensu <enrico.suarez@xsplit.com>
2023-06-12 18:49:14 +0800

    chore: generate md links into commit msgs for discord changebot

---
+++ tools/src/commands/commit-content.ts
@@ -0,0 +1,56 @@
+import { spawnSync } from "child_process";
+import { Command } from "commander";
+import { groupBy, keyBy } from "lodash-es";
+import { temporaryFile, temporaryFileTask } from "tempy";
+import * as frontmatter from "../lib/frontmatter.js";
+import { readFileSync, writeFileSync } from "fs";
+
+const commitContent = new Command('commit-content');
+
+commitContent
+  .action(async () => {
+    const url = new URL("https://bastion-falls.thekennel.info");
+    const { stdout } = spawnSync('git', ['diff', '--name-status', '-r', 'HEAD']);
+    const changes = stdout.toString().split("\n").map(change => {
+      const match = change.match(/^(\w)\s+(content\/.+)/);
+      if (match?.length === 3) {
+        const [, typeItem, path] = match;
+
+        let type;
+        switch (typeItem.toLowerCase()) {
+          case 'm':
+            type = 'modified';
+            break;
+          case 'a':
+            type = 'added';
+            break;
+        }
+        return {
+          type,
+          path
+        };
+      }
+    })
+
+    const oldContent = readFileSync('.git/COMMIT_EDITMSG').toString();
+
+    const filtered = groupBy(changes.filter((i): boolean => i !== undefined), 'type')
+    let fileContent = ''
+    for (const [key, values] of Object.entries(filtered).sort()) {
+      fileContent += `# ${key}\n`;
+      for (const value of values) {
+        const { path } = value ?? {};
+        if (path) {
+          url.pathname = path.replace('content', '');
+          const { data } = await frontmatter.read(path);
+
+          fileContent += ` - [${data.title}](${url.toString()})\n`
+        }
+      }
+    }
+
+    fileContent += `\n${oldContent}`
+    writeFileSync('.git/COMMIT_EDITMSG', fileContent);
+  });
+
+export default commitContent;
--- tools/src/index.ts
+++ tools/src/index.ts
@@ -4,6 +4,7 @@ import migrate from "./commands/migrate.js";
 import transformFrontmatter from "./commands/transform-frontmatter.js";
 import newFile from "./commands/new.js";
 import generateFamily from "./commands/gen-family.js";
+import commitContent from "./commands/commit-content.js";
 
 const app = new Command('repo-tools');
 
@@ -13,6 +14,7 @@ export async function main() {
   app.addCommand(transformFrontmatter);
   app.addCommand(newFile);
   app.addCommand(generateFamily);
+  app.addCommand(commitContent)
 
   await app.parseAsync();
 }
