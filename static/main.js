2295c6574678355aede0fdc40da5dedd5edf044a
ensu <enrico.suarez@xsplit.com>
2023-06-19 22:05:21 +0800

    feat: added burger to hide menu

--- static/main.js
+++ static/main.js
@@ -1,13 +1,30 @@
-function onLoad(event) {
-  const nav = document.getElementsByName("nav-section");
-  nav.forEach((elem) => {
-    elem.addEventListener("change", (event) => {
-      nav.forEach((search) => {
-        if (elem !== search) {
-          search.checked = false;
-        }
-      });
-    });
+function hideSidebar() {
+  const burger = document.getElementById("burger");
+  const sidebar = document.getElementById("sidebar-container");
+  burger.addEventListener("click", function () {
+    console.log("Click");
+    if (
+      sidebar.hasAttribute("data-display") &&
+      sidebar.getAttribute("data-display") == "show"
+    ) {
+      sidebar.setAttribute("data-display", "hidden");
+    } else {
+      sidebar.setAttribute("data-display", "show");
+    }
   });
+}
+function onLoad(event) {
+  // const nav = document.getElementsByName("nav-section");
+  // nav.forEach((elem) => {
+  //   elem.addEventListener("change", (event) => {
+  //     nav.forEach((search) => {
+  //       if (elem !== search) {
+  //         search.checked = false;
+  //       }
+  //     });
+  //   });
+  // });
+
+  hideSidebar();
 }
 document.addEventListener("DOMContentLoaded", onLoad);
